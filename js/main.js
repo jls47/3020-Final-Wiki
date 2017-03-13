6dd9f75369bac148db66e922c7bc3d94aca3d5e5
John Luke Schaefer <lukeschaefer@gmail.com>
2017-03-13 00:02:15 -0700

    whoopsie

--- js/main.js
+++ js/main.js
@@ -316,7 +316,7 @@ var topo = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/til
 satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2NoYWVmMTYiLCJhIjoiY2l5bDE1aHZ5MDAydTJ3bnpoaWZqbWpkcSJ9.ip2r4oOtxQdBmGDcL7IaWA', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 	id: 'your.mapbox.project.id'
-}),
+});
 
 //put them all into an object to be included into the map:
 var baseMaps = {
